fc9fc82dac8bedbdfb45821c62cdc45d5ab6573f
trustpizza <maxelolsson@gmail.com>
2022-12-15 13:12:47 -0600

    Friends now are able to accept and deny requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,12 @@ class User < ApplicationRecord
 
   has_many :began_friendships, foreign_key: :user_id, class_name: "Friendship", dependent: :destroy
   has_many :accepted_friendships, foreign_key: :friend_id, class_name: "Friendship", dependent: :destroy
-  
+  has_many :began_friends, through: :began_friendships, source: :friend, dependent: :destroy
+  has_many :accepted_friends, through: :accepted_friendships, source: :user, dependent: :destroy
+
+
+
+  def friends
+    accepted_friends + began_friends
+  end
 end
