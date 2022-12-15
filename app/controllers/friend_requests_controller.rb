238c90563580b5faf29a2e19c351d9c42007b72a
trustpizza <maxelolsson@gmail.com>
2022-12-15 10:47:09 -0600

    Add restful routes to FR controller

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,18 @@
 class FriendRequestsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def friend_request_params
+  end
 end
