2a0d69ab0fc8086c12d5ae5de1b78a896505b4c5
trustpizza <maxelolsson@gmail.com>
2022-12-15 12:27:06 -0600

    Create CRUD actions for friendship controller, needs destroyer

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,34 @@
 class FriendshipsController < ApplicationController
+  def index
+    @friendships = Friendships.ApplicationController
+    @user = User.find(params[:user_id])
+  end
+
+  def new
+    @friendship = Friendship.new
+  end
+
+  def create
+    @friendship = current_user.accepted_friendships.build(friendship_params)
+    return unless @friendship.save
+
+    # Destroy friend_request
+    redirect_to root_url
+  end
+
+  def destroy
+    @friendship = Friendship.find_by(user_id: friendship_params[:user_id],
+                                    friend_id: friendship_params[:friend_id]) ||
+                  Friendship.find_by(user_id: friendship_params[:friend_id],
+                                    friend_id: friendship_params[:user_id])
+
+    return unless @friendship.destroy
+
+    redirect_to root_url
+  end
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id)
+  end
 end
