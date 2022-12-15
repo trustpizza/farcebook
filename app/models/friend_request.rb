1b7a4d3e2c2b289e6b2f52572f0915d6dcb6fb20
trustpizza <maxelolsson@gmail.com>
2022-12-14 19:31:21 -0600

    Create Fr-User model relationships

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,2 +1,4 @@
 class FriendRequest < ApplicationRecord
+  belongs_to :sender, class_name: 'User'
+  belongs_to :reciever, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :sent_friend_requests, foreign_key: 'sender_id', class_name: 'FriendRequest', dependent: :destroy 
+  has_many :received_friend_requests, foreign_key: 'receiver_id', class_name: 'FriendRequest', dependent: :destroy 
 end
