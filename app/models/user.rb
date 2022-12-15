5ff4a8753b000f413edbf2a46c1f9164ee690218
trustpizza <maxelolsson@gmail.com>
2022-12-15 09:02:33 -0600

    Finalize devise registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  
   has_many :sent_friend_requests, foreign_key: 'sender_id', class_name: 'FriendRequest', dependent: :destroy 
   has_many :received_friend_requests, foreign_key: 'receiver_id', class_name: 'FriendRequest', dependent: :destroy 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_031912) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_145611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_031912) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "friend_requests", "users", column: "request_reciever_id"
