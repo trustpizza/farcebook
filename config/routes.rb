4e07b6dde647813e8517a1022ccccbcdb3154144
trustpizza <maxelolsson@gmail.com>
2022-12-15 09:22:59 -0600

    Configure sign up params to include username, firstname and lastname

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
+  #  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
