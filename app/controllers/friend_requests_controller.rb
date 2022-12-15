c4d747b266b5aee3d11685bfb2a675c957a4b79b
trustpizza <maxelolsson@gmail.com>
2022-12-15 10:50:40 -0600

    Create index, new, create, destroy routes.  Needs redirects

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,18 +1,29 @@
 class FriendRequestsController < ApplicationController
   def index
+    @friend_requests = FriendRequest.all
   end
 
   def new
+    @friend_request = FriendRequest.new
   end
 
   def create
+    @friend_request = current_user.sent_friend_requests.build(friend_request_params)
+    return unless @friend_request.save
+    
+    #needs redirect
   end
 
   def destroy
+    @friend_request = FriendRequest.find_by(friend_request_params)
+    return unless @friend_request.destroy
+
+    #needs redirect
   end
 
   private
 
   def friend_request_params
+    params.require(:friend_request).permit(:sender_id, :receiver_id)
   end
 end
