d9d006937f9c2390d88a6a36c0add4e12c56a4f9
trustpizza <maxelolsson@gmail.com>
2022-12-16 10:56:14 -0600

    Fill out likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,22 @@
 class LikesController < ApplicationController
-  def 
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @post = Post.find(params[:posts_id])
+    @like = @post.likes.create(like_params)
+  end
+
+  def destroy
+    @post = Pst.find(params[:posts_id])
+    @like = @post.likes.find_by(like_params)
+    @like.destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :posts_id)
+  end
 end
