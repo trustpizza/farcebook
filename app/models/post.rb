263f7bdbfe225b8bd63afb994e47bdac3f81ed7c
trustpizza <maxelolsson@gmail.com>
2022-12-16 08:51:35 -0600

    Validate posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+  # Validations 
+  validates :body, presence: true, length: { maximum: 300 }
+  validates :title, presence: true
+
+  # Associations
   belongs_to :author, class_name: "User"
 end
